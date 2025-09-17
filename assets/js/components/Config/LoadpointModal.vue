af3678df4f93a439c3ed05746dc5952035654d20
bangu <banggu@weyland-x.com>
2025-09-17 17:28:56 +0800

    fix(SelectGroup): 修正选项类型支持字符串和数字
    refactor(LoadpointModal): 将阶段选项值从字符串改为数字
    
    test(config-loadpoint): 添加对负载点默认值的验证测试

--- assets/js/components/Config/LoadpointModal.vue
+++ assets/js/components/Config/LoadpointModal.vue
@@ -713,9 +713,9 @@ export default {
 		},
 		phasesOptions() {
 			return [
-				{ value: "1", name: this.$t("config.loadpoint.phases1p") },
-				{ value: "3", name: this.$t("config.loadpoint.phases3p") },
-			];
+				{ value: 1, name: this.$t("config.loadpoint.phases1p") },
+				{ value: 3, name: this.$t("config.loadpoint.phases3p") },
+			];	
 		},
 		showCircuit() {
 			return this.circuits.length > 0;
--- assets/js/components/Helper/SelectGroup.vue
+++ assets/js/components/Helper/SelectGroup.vue
@@ -25,8 +25,7 @@ export default defineComponent({
 	name: "SelectGroup",
 	props: {
 		id: String,
-		options: Array as PropType<SelectOption<string>[]>,
-		modelValue: [Number, String, Boolean, null],
+		options: Array as PropType<SelectOption<string | number>[]>,		modelValue: [Number, String, Boolean, null],
 		equalWidth: Boolean,
 		large: Boolean,
 		transparent: Boolean,
